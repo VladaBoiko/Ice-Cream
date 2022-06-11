f514167df9871539db20ab296aaa336249aea4b4
VladaBoiko <bermezha@gmail.com>
2022-06-11 19:52:53 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,10 +8,6 @@
     closeCowModalBtn: document.querySelector('.js-cow-modal-close'),
     modalCow: document.querySelector('.js-cow-modal'),
 
-    openLocationModalBtn: document.querySelector('.js-location-modal-open'),
-    closeLocationModalBtn: document.querySelector('.js-location-modal-close'),
-    modalLocation: document.querySelector('.js-location-modal'),
-
     openFranchiseModalBtn: document.querySelector('.js-franchise-modal-open'),
     closeFranchiseModalBtn: document.querySelector('.js-franchise-modal-close'),
     modalFranchise: document.querySelector('.js-franchise-modal'),
@@ -27,10 +23,6 @@
     refs.closeCowModalBtn.addEventListener('click', toggleCowModal);
   }
 
-  if (refs.openLocationModalBtn && refs.closeLocationModalBtn) {
-    refs.openLocationModalBtn.addEventListener('click', toggleLocationModal);
-    refs.closeLocationModalBtn.addEventListener('click', toggleLocationModal);
-  }
   if (refs.openFranchiseModalBtn && refs.closeFranchiseModalBtn) {
     refs.openFranchiseModalBtn.addEventListener('click', toggleFranchiseModal);
     refs.closeFranchiseModalBtn.addEventListener('click', toggleFranchiseModal);
@@ -42,10 +34,6 @@
   }
   function toggleCowModal() {
     refs.modalCow.classList.toggle('is-hidden');
-    refs.body.classList.toggle('noscroll');
-  }
-  function toggleLocationModal() {
-    refs.modalLocation.classList.toggle('is-hidden');
     refs.body.classList.toggle('noscroll');
   }
   function toggleFranchiseModal() {

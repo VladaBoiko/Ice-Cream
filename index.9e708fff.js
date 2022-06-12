3f810305af8b32f349ca39da211d59fd2b3cf860
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2022-06-12 14:29:46 +0000

    Deploying to gh-pages from @ VladaBoiko/Ice-Cream@bc8ea65de03155788ab1e6c07a9ba184fbca662f 🚀

--- index.9e708fff.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slider1").slick({arrows:!1,dots:!1,fade:!0,asNavFor:".slider2,.slider3",autoplay:!0,autoplaySpeed:8e3}),$(".slider2").slick({arrows:!1,dots:!0,fade:!0,asNavFor:".slider1,.slider3"}),$(".slider3").slick({arrows:!1,dots:!0,fade:!0,asNavFor:".slider1,.slider2"})}));
-//# sourceMappingURL=index.9e708fff.js.map

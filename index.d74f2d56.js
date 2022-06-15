7f65113609406cd399c10e12b013a65db43ed9f4
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2022-06-15 11:50:16 +0000

    Deploying to gh-pages from @ VladaBoiko/Ice-Cream@1dd56276be87feea114d4ba70e6cb351f517a417 🚀

--- index.d74f2d56.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openHeaderModalBtnTwo:document.querySelector(".js-header-modal-open-mobile"),closeHeaderModalBtnTwo:document.querySelector(".js-header-modal-close-mobile"),modalHeader:document.querySelector(".js-header-modal-mobile"),body:document.querySelector("body")};function o(){e.modalHeader.classList.toggle("is-hidden"),e.body.classList.toggle("noscroll")}e.openHeaderModalBtnTwo.addEventListener("click",o),e.closeHeaderModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.d74f2d56.js.map
---
+++ index.e9542ab7.js
@@ -0,0 +1,2 @@
+!function(){var e={openHeaderModalBtnTwo:document.querySelector(".js-header-modal-open-mobile"),modalHeader:document.querySelector(".js-header-modal-mobile"),body:document.querySelector("body")};e.openHeaderModalBtnTwo.addEventListener("click",(function(){e.modalHeader.classList.toggle("is-hidden"),e.body.classList.toggle("noscroll")}))}();
+//# sourceMappingURL=index.e9542ab7.js.map

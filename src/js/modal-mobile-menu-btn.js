1dd56276be87feea114d4ba70e6cb351f517a417
VladaBoiko <bermezha@gmail.com>
2022-06-15 14:49:26 +0300

    fix

--- src/js/modal-mobile-menu-btn.js
+++ src/js/modal-mobile-menu-btn.js
@@ -3,15 +3,15 @@
     openHeaderModalBtnTwo: document.querySelector(
       '.js-header-modal-open-mobile'
     ),
-    closeHeaderModalBtnTwo: document.querySelector(
-      '.js-header-modal-close-mobile'
-    ),
+    // closeHeaderModalBtnTwo: document.querySelector(
+    //   '.js-header-modal-close-mobile'
+    // ),
     modalHeader: document.querySelector('.js-header-modal-mobile'),
     body: document.querySelector('body'),
   };
 
   refs.openHeaderModalBtnTwo.addEventListener('click', toggleHeaderModal);
-  refs.closeHeaderModalBtn.addEventListener('click', toggleHeaderModal);
+  // refs.closeHeaderModalBtn.addEventListener('click', toggleHeaderModal);
 
   function toggleHeaderModal() {
     refs.modalHeader.classList.toggle('is-hidden');

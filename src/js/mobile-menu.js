21c7a64acfec8c8ee7dcd825b395415411ac8b79
VladaBoiko <bermezha@gmail.com>
2022-06-15 14:38:24 +0300

    fix

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -24,6 +24,7 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    // bodyScrollLock.enableBodyScroll(document.body);
+    body.classList.remove("noscroll");
   });
 })();
